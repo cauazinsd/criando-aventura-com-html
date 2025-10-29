778cd59cf8a08072f966bcac6f89fe7f719dbfe8
cauazinsd <caua.souza.lopes@escola.pr.gov.br>
2025-10-29 09:00:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const avanca = document.querySelectorAll('.btn-proximo');
 
+avanca.forEach(button => ) {
+button.addEventListener('click', function(){
+const atual = document.querySeletor('ativo');
+const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+atual.classlist.remove('ativo');
+document.getElementaryById(proximoPasso).classlist.add('ativo');
+  })
+})
